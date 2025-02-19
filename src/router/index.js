d1b2b3d3b4b52bc27fd89c08450ee0e6c458896c
tdy <2015214273@qq.com>
2025-02-19 11:48:42 +0800

    集成vue-router插件

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      redirect: '/search'
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      // 使用路由懒加载
+      component: () => import('../views/SearchView.vue')
+    }
+  ]
 })
 
 export default router
